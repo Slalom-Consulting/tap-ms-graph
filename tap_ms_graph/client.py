ee01c621c84ac4f24cd35a475022472d7852e27a
Robby Robinson <robby.robinson@slalom.com>
2023-05-09 09:57:27 -0700

    added stringify complex types

--- tap_ms_graph/client.py
+++ tap_ms_graph/client.py
@@ -1,5 +1,7 @@
 """REST client handling, including MSGraphStream base class."""
 
+from __future__ import annotations
+
 import json
 import uuid
 from pathlib import Path
@@ -152,3 +154,13 @@ class MSGraphStream(RESTStream):
             child_context = record[self.record_child_context]
 
         return child_context
+    
+    def post_process(self, row: dict, context: dict | None = None) -> dict | None:
+        #converts complex types to string
+
+        new_row = row.copy()
+        for k, v in row.items():
+            if isinstance(v, dict) or isinstance(v, list):
+                new_row[k] = json.dumps(v)
+
+        return new_row
