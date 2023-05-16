41a8c0f8bb9b53c6d21443b4be98ae27a03be8a2
Robby Robinson <robby.robinson@slalom.com>
2023-05-16 10:50:09 -0700

    added teams streams

--- tap_ms_graph/streams.py
+++ tap_ms_graph/streams.py
@@ -26,6 +26,42 @@ class GroupMembersStream(MSGraphChildStream):
     }
 
 
+class TeamsStream(MSGraphChildStream):
+    parent_stream_type = GroupsStream
+    name = "teams"
+    path = "/teams/{group_id}"
+    primary_keys = ["id"]
+    odata_context = "teams/$entity"
+    child_context = {"id": "team_id"}
+    #$filter=resourceProvisioningOptions/any(i:i eq 'Team')
+
+
+class TeamChannelsStream(MSGraphChildStream):
+    parent_stream_type = TeamsStream
+    name = "teamChannels"
+    path = "/teams/{team_id}/channels"
+    primary_keys = ["team_id", "id"]
+    odata_context = "#teams/channels"
+    child_context = {"id": "channel_id"}
+
+    parent_context_schema = {
+        "team_id": {"type": "string"},
+    }
+
+class TeamChannelMembersStream(MSGraphChildStream):
+    parent_stream_type = TeamChannelsStream
+    name = "groups"
+    path = "/teams/{team_id}/channel/{channel_id}/members"
+    primary_keys = ["team_id", "channel_id", "id"]
+    odata_context = "teams/$entity"
+    #$filter=resourceProvisioningOptions/any(i:i eq 'Team')
+
+    parent_context_schema = {
+        "team_id": {"type": "string"},
+    }
+
+
+
 class SubscribedSkusStream(MSGraphStream):
     name = "subscribedSkus"
     path = "/subscribedSkus"
